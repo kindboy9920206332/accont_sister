a7730a171aae214045a7469347bdf6b6a7a63dd0
kindboy <kindboy2025@gmail.com>
2024-12-15 13:08:29 +0330

    component_pannel

--- src/app/Accont/Accont_Home.jsx
+++ src/app/Accont/Accont_Home.jsx
@@ -1,19 +1,54 @@
+"use client";
+import { createContext, useEffect, useState } from "react";
 import Accont_Pannel_meno from "./Accont_pannel/Accont_pannel";
 import Personal_Accont from "./Accont_personall/personall";
-
+export const PanelContext = createContext();
 export default function Accont_Home() {
+  const [secen_pennel, set_secen_pannel] = useState();
+  const secen_pannels_name = [
+    {
+      id: 1,
+      name_pannel: "Personal Data",
+      component: <Personal_Accont></Personal_Accont>,
+    },
+    {
+      id: 2,
+
+      name_pannel: "Payment & Instalments",
+      component: <Personal_Accont></Personal_Accont>,
+    },
+    {
+      id: 3,
+      name_pannel: "Orders",
+      component: <Personal_Accont></Personal_Accont>,
+    },
+  ];
   return (
     <>
-      <div className="w-[100%] h-[86%] absolute bottom-0 flex justify-center  ">
-        <div className="w-[88%] h-[100%] flex relative justify-between items-start bottom-0 ">
-          <div className=" inline-flex py-[20px] px-[2px] bg-gray-100 rounded-[7px] w-[250px] h-[550px]">
-            <Accont_Pannel_meno></Accont_Pannel_meno>
-          </div>
-          <div className="w-[74%] h-[100%]">
-            <Personal_Accont></Personal_Accont>
+      <PanelContext.Provider
+        value={{
+          FU_set_pannel: (name_pannel) => {
+            set_secen_pannel(name_pannel);
+          },
+        }}
+      >
+        <div className="w-[100%] h-[86%] absolute bottom-0 flex justify-center  ">
+          <div className="w-[88%] h-[100%] flex relative justify-between items-start bottom-0 ">
+            <div className=" inline-flex py-[20px] px-[2px] bg-gray-100 rounded-[7px] w-[250px] h-[550px]">
+              <Accont_Pannel_meno></Accont_Pannel_meno>
+            </div>
+            <div className="w-[74%] h-[100%]">
+              {secen_pannels_name.map((item, index) => {
+                if (item.name_pannel === secen_pennel) {
+                  return <div key={index}>{item.component}</div>;
+                } else {
+                  return null;
+                }
+              })}
+            </div>
           </div>
         </div>
-      </div>
+      </PanelContext.Provider>
     </>
   );
 }
--- src/app/Accont/Accont_pannel/Accont_pannel.jsx
+++ src/app/Accont/Accont_pannel/Accont_pannel.jsx
@@ -1,76 +1,76 @@
 "use client";
-import Link from "next/link";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { PanelContext } from "../Accont_Home";
 export default function Accont_Pannel_meno() {
+  const { FU_set_pannel } = useContext(PanelContext);
   const [item_meno, set_item_meno] = useState([
     {
       porofile: "Jimmy smith",
       img: "/svgs/profile-circle.svg",
       class: "text-black  font-bold",
-      click_go_page: "/personal",
       contain: false,
     },
     {
-      porofile: "Personal Data ",
+      porofile: "Personal Data",
       img: "/svgs/user-edit.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "personal",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Payment & Instalments",
       img: "/svgs/dollar-circle.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "Payment",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Orders",
       img: "/svgs/bag.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "Orders",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Wish list",
       img: "/svgs/heart.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "Wish list",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Discounts",
       img: "/svgs/gift.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "Discounts",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Security & access",
       img: "/svgs/security-safe.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "Security",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Notification",
       img: "/svgs/notification.svg",
-      class: "text-gray-700 hover:text-blue-500 ",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer ",
+      click_go_page: "Notification",
       contain: false,
       class2: "text-blue-500 ",
     },
     {
       porofile: "Contact us",
       img: "/svgs/24-support.svg",
-      class: "text-gray-700 hover:text-blue-500",
-      click_go_page: "#",
+      class: "text-gray-700 hover:text-blue-500 cursor-pointer",
+      click_go_page: "Contact",
       contain: false,
       class2: "text-blue-500 ",
     },
@@ -78,7 +78,7 @@ export default function Accont_Pannel_meno() {
       porofile: "Log out",
       img: "/svgs/logout.svg",
       class: "text-red-600",
-      click_go_page: "#",
+      click_go_page: "Log_out",
       contain: false,
     },
   ]);
@@ -92,14 +92,12 @@ export default function Accont_Pannel_meno() {
               const new_item = [...item_meno];
               new_item.map((item, i) => {
                 if (index === i && index != 0 && index != new_item.length - 1) {
-                  console.log(i);
                   new_item[i] = { ...item, contain: true };
                 } else {
                   new_item[i] = { ...item, contain: false };
                 }
               });
               set_item_meno(() => new_item);
-              console.log(new_item);
             }}
             key={index}
             className={
@@ -109,11 +107,12 @@ export default function Accont_Pannel_meno() {
             }
           >
             <img src={item.img} />
-            <Link href={item.click_go_page}>
-              <span className={!item.contain ? item.class : item.class2}>
-                {item.porofile}
-              </span>
-            </Link>
+            <span
+              onClick={() => FU_set_pannel(item.porofile)}
+              className={!item.contain ? item.class : item.class2}
+            >
+              {item.porofile}
+            </span>
           </div>
         ))}
       </div>
